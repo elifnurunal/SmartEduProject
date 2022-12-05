3c356042a1a6a7143ce2c7ac1d970c92ff8b6788
Elif Ünal <eliffunll6@gmail.com>
2022-12-05 14:52:56 +0300

    lesson 02

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ const express = require("express");
 
 const app = express();
 
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+
+// MIDDLEWARES
+app.use(express.static("public"));
+
+
+// ROUTES
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX PAGE');
+  res.status(200).render("index" , {
+    page_name: "index"
+  });
 });
+
+app.get('/about', (req, res) => {
+    res.status(200).render("about", {
+        page_name: "about"
+    });
+  });
+
 
 const port = 3000;
 app.listen(port, () => {
