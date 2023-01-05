a0d89a636577a8d7c4d47ba1d8bfd91bb623b9c6
Elif Ünal <eliffunll6@gmail.com>
2023-01-05 11:45:59 +0300

    first commit

--- app.js
+++ app.js
@@ -1,28 +1,69 @@
-const express = require("express");
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
-// TEMPLATE ENGINE
-app.set("view engine", "ejs");
+// CONNECT DB
+mongoose
+  .connect('mongodb://127.0.0.1/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connected succesfully');
+  });
 
+mongoose.set('strictQuery', true);
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// GLOBAL VARİABLE
+global.userIN = null;
 
 // MIDDLEWARES
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1/smartedu-db' }),
+  })
+);
 
-
-// ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render("index" , {
-    page_name: "index"
-  });
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
 });
 
-app.get('/about', (req, res) => {
-    res.status(200).render("about", {
-        page_name: "about"
-    });
-  });
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
+// ROUTES
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
+app.use('/login', pageRoute);
 
 const port = 3000;
 app.listen(port, () => {
